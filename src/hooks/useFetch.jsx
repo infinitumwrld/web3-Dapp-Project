31c84e27afcf276d389d9e72b5973e7cc9b486ff
ysk <103978285+infinitumwrld@users.noreply.github.com>
2023-02-02 13:28:51 -0500

    Update useFetch.jsx
    code explanation updated.

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -24,3 +24,13 @@ const useFetch = ({ keyword }) => {
 }
 
 export default useFetch;
+
+/*
+CODE EXPLANATION:
+This is a React custom hook that fetches a GIF from the Giphy API based on a keyword. The hook uses the "useEffect" and "useState" hooks from the React library. 
+The API_KEY is imported from the environment variables and is used to authenticate the API request. The hook returns the URL of the GIF, which is stored in the 
+component's state using the "setGifUrl" function. When the component that uses this hook is mounted, the "useEffect" hook will run the "fetchGifs" function, which sends a 
+GET request to the Giphy API to retrieve a GIF based on the keyword. If the request is successful, the URL of the GIF is set in the component's state using the "setGifUrl" function. 
+If an error occurs, the hook will set a default GIF URL as a fallback.
+
+*/
