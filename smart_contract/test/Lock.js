8dcff226c50c2029f3ac5cd4c6e9f626263eb2f3
ysk <103978285+infinitumwrld@users.noreply.github.com>
2023-02-02 15:53:57 -0500

    Update Lock.js
    updated code

--- smart_contract/test/Lock.js
+++ smart_contract/test/Lock.js
@@ -6,7 +6,7 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
+  // We define a fixture to reuse the same setup in every test. // we define it
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearLockFixture() {
